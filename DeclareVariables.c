adca5d10e727d75af9de1db8e266d6a20367194a
CliveMinlay <cliveminlay26@gmail.com>
2025-11-13 21:12:11 +0800

    add the other code

---
+++ DeclareVariables.c
@@ -0,0 +1,20 @@
+//MINLAY, CLIVE DUSTINE M. DIT 1-2
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    float height = 5.5;
+    double weight = 55.2341;
+    char grade = 'D';
+    char name [] = "KLAYB";
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Weight: %.4lf\n", weight);
+    printf("Grade: %c\n", grade);
+    printf("Name: %s\n", name);
+
+
+
+    return 0;
+}
